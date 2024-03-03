91dba3ca077b18959ea92584dd28aa003e4c85bd
Riyadh08 <robiulriyadh66@gmail.com>
2024-03-03 16:51:02 +0600

    Add project files.

---
+++ DemoContact/main.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="main.aspx.cs" Inherits="DemoContact.main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <form id="form1" runat="server">
+         <section id="contact">
+            <p class="section_text_p1">Get in Touch</p>
+            <h1 class="title">Contact Me</h1>
+
+            <asp:TextBox ID="nameTextBox" runat="server" CssClass="contact-input" placeholder="Your name" Required="true"></asp:TextBox>
+            <br />
+            <br />
+            <asp:TextBox ID="emailTextBox" runat="server" CssClass="contact-input" placeholder="Your email" Required="true" TextMode="Email"></asp:TextBox>
+            <br />
+            <br />
+            <asp:TextBox ID="messageTextBox" runat="server" CssClass="contact-input" placeholder="Write message" TextMode="MultiLine" Rows="6" Required="true"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="txtSubmit" runat="server" Text="Send" OnClick="txtSubmit_Click" CssClass="contact-btn" />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        </section>
+        <h2 class="heading">Contact<span>Me</span></h2>
+            <section class="contact" id="contacts">
+
+
+                <div>
+                    <asp:TextBox runat="server" ID="name" placeholder="Fullname" CssClass="input-box" />
+
+                </div>
+
+                <div>
+                    <asp:TextBox runat="server" ID="email" placeholder="Email" TextMode="Email" CssClass="input-box" />
+                </div>
+
+                <div>
+                    <asp:TextBox runat="server" ID="phone" placeholder="Phone Number" TextMode="Phone" CssClass="input-box" />
+
+                </div>
+                <div>
+                    <asp:TextBox runat="server" ID="subject" placeholder="Subject" CssClass="input-box" />
+                </div>
+                <asp:TextBox runat="server" ID="message" placeholder="Your Message to me" TextMode="MultiLine" CssClass="input-box" />
+                <asp:Button runat="server" ID="Button1" Text="Send" CssClass="btn" OnClick="txtSubmit_Click" />
+
+                <asp:Label runat="server" ID="lblError" Text="" />
+
+            </section>
+    </form>
+</body>
+</html>
---
+++ DemoContact/main.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DemoContact
+{
+    public partial class main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        String strcon = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+      
+        protected void txtSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sendemail();
+                SqlConnection con = new SqlConnection(strcon);
+                con.Open();
+                SqlCommand com = new SqlCommand("INSERT INTO message ([name],[email],[phone],[subject],[message]) VALUES(@name,@email,@phone,@subject,@message)", con);
+                com.Parameters.AddWithValue("@name", name.Text.Trim());
+                com.Parameters.AddWithValue("@email", email.Text.Trim());
+                com.Parameters.AddWithValue("@phone", phone.Text.Trim());
+                com.Parameters.AddWithValue("@subject", subject.Text.Trim());
+                com.Parameters.AddWithValue("@message", message.Text.Trim());
+                com.ExecuteNonQuery();
+                con.Close();
+                name.Text = "";
+                email.Text = "";
+                message.Text = "";
+                phone.Text = "";
+                subject.Text = "";
+
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+
+
+            }
+        }
+        private void sendemail()
+        {
+            String smtpUserName;
+            String smtpPassword;
+            MailMessage mail = new MailMessage();
+            SmtpClient smtp_Client = new SmtpClient(System.Configuration.ConfigurationSettings.AppSettings["smtpClient"]);
+
+            smtpUserName = System.Configuration.ConfigurationSettings.AppSettings["smtpUserName"];
+            smtpPassword = System.Configuration.ConfigurationSettings.AppSettings["smtpPassword"];
+            mail.From = new MailAddress(smtpUserName);
+            mail.To.Add(email.Text.Trim());
+            mail.Subject = "your Subject";
+            mail.Body = ("Name: " + name.Text.Trim() + " Email: " + email.Text.Trim() + "Message: " + message.Text.Trim());
+            smtp_Client.Port = Convert.ToInt32(System.Configuration.ConfigurationSettings.AppSettings["smtpPort"]);
+            smtp_Client.Credentials = new System.Net.NetworkCredential(smtpUserName, smtpPassword);
+            smtp_Client.EnableSsl = Convert.ToBoolean(System.Configuration.ConfigurationSettings.AppSettings["enableSSL"]);
+            smtp_Client.Send(mail);
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
+}
